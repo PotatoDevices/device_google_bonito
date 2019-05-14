e8e0160e0961adec9c2f156ca8ee5c6ffd1ee56c
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-14 00:36:02 -0700

    b4s4: Set up proprietary files extraction & build vendor image
    * Blobs that can be built from source code are built from source, except
      librmnetctl.
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    * Various lib and folder symlinks need to be created.
    
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
    Co-authored-by: Christopher N. Hesse <raymanfx@gmail.com>
    Co-authored-by: Dan Pasanen <dan.pasanen@gmail.com>
    Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
    Co-authored-by: Josh Chasky <jchasky@gmail.com>
    Co-authored-by: "Josh Fox (XlxFoXxlX)" <joshfox87@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
    Co-authored-by: razorloves <razorloves@gmail.com>
    Change-Id: I17f79b417a8f0d022bec9c6b5f37d03b842d43e1

---
+++ bonito/BoardConfigPotato.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2018-2021 The Potato Open Sauce Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include device/google/bonito/BoardConfigPotato.mk
+
+include vendor/google/bonito/BoardConfigVendor.mk
